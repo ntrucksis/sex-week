7e1b0afa662cb82ed044caa8add3b07f40aefcd9
Nelson Okunlola <nelly@nellys-tool.local>
2020-04-26 21:22:35 -0500

    added components, fixed index.html

--- sex-week/src/app/app.module.ts
+++ sex-week/src/app/app.module.ts
@@ -4,11 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BannerComponent } from './banner/banner.component';
+import { FooterComponent } from './footer/footer.component';
+import { SpotifyComponent } from './spotify/spotify.component';
+import { StreamsComponent } from './streams/streams.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent,
+    BannerComponent,
+    FooterComponent,
+    SpotifyComponent,
+    StreamsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
---
+++ sex-week/src/app/navbar/navbar.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  goToEvents(): void {
+    this.router.navigate(["/events"]);
+  }
+
+}
