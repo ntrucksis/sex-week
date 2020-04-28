03fae0105f9d230d95e946913eae723dce3cf3de
Nelson Okunlola <nelly@nellys-tool.local>
2020-04-27 21:42:31 -0500

    navbar tweak. temp banner image. /pics -> /images. spotify -> spotify-social. social icons tweak

--- sex-week/src/app/navbar/navbar.component.ts
+++ sex-week/src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, HostListener } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 import { Router } from '@angular/router';
 
@@ -24,5 +24,26 @@ export class NavbarComponent implements OnInit {
   goHome(): void {
     this.router.navigate(["/home"]);
   }
+
+
+
+  // ** This should have been in JQuery but I had an Issue with getting JQuery to work with Angular -Nelly//
+
+
+  @HostListener('window:scroll', ['$event']) // for window scroll events
+  onScroll(event) {
+
+      //After scrolling 100px from the top...
+      console.log("Scroll top is", window.pageYOffset, "young nelly");
+      console.log("Nav is ", elementNav);
+      var elementNav = document.getElementById("Nav");
+      if ((window.pageYOffset >= 60)) {
+          console.log("scrolling")
+          elementNav.classList.add("transparent");
+      } else {
+          console.log("Top")
+          elementNav.classList.remove("transparent");
+      }
+  };
 
 }
